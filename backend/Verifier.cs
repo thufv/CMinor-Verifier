594c01bdb14c75a18afc4dffdfdc6d82bdc2cfe8
namasikanam <namasikanam@gmail.com>
2022-08-04 10:50:29 +0800

    template Verifier.cs

---
+++ backend/Verifier.cs
@@ -0,0 +1,34 @@
+/*
+    TODO: 这是你唯一允许编辑的文件，你可以对此文件作任意的修改，只要整个项目可以正常地跑起来。
+*/
+
+using System.IO;
+
+namespace cminor
+{
+    /// <summary> 一个验证器，接受一个中间表示，判断其是否有效。 </summary>
+    class Verifier
+    {
+        SMTSolver solver = new SMTSolver();
+        TextWriter writer;
+
+        public Verifier(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        /// <summary> 应用此验证器 </summary>
+        /// <param name="cfg"> 待验证的控制流图 </param>
+        /// <returns> 验证结果 </returns>
+        /// <list type="bullet">
+        ///     <item> “&gt; 0” 表示所有的 specification 都成立 </item>
+        ///     <item> “&lt; 0” 表示有一个 specification 不成立 </item>
+        ///     <item> “= 0” 表示 unknown </item>
+        /// </list>
+        public int Apply(IRMain cfg)
+        {
+            return 0;
+        }
+    }
+}
+
